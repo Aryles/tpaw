f45c17a0b82f1f2669e4ec42f2ffdfe3a2b423e8
Aryles <talbinearyles@gmail.com>
2018-11-14 15:58:27 +0100

    Final

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -2,22 +2,22 @@
 $( document ).ready(function() {
      console.log( "DOM ready!" );
 
-     $("#formulaire").on("click",function(event){ 
+     $("#envoi").on("click",function(event){ 
         event.preventDefault(); 
         console.log( "click" ); 
 
-        if ($('#nom').val().length < 5 ) { 
+        if ($('#nom').val().length < 5|| $('#nom').val() == "" ) { 
         $('#myModal').modal("show");
         $(".modal-title").text("Message erreur");
         $(".modal-body").text("veuillez renseignez le champs nom");
       }
-      else if ($('#prenom').val().length < 5 ) { 
+      else if ($('#prenom').val().length < 5|| $('#prenom').val() == "" ) { 
         $('#myModal').modal("show");
         $(".modal-title").text("Message erreur");
         $(".modal-body").text("veuillez renseignez le champs prenom");
     }
     
-    else if ( $('#dn').val() == "") { 
+    else if ( $('#dn').val() == ""|| $('#dn').val() == "") { 
         $('#myModal').modal("show");
         $(".modal-title").text("Message erreur");
         $(".modal-body").text("veuillez renseignez le champs date de naissance");
@@ -36,10 +36,9 @@ $( document ).ready(function() {
     }
     else {
         $('#myModal').modal("show");
-        $(".modal-title").text( "Bienvenue  "+ document.querySelector("#prenom").val());
-        $(".modal-body").text( "vous etes nés le"+ document.querySelector("#dn").val()+" et vous habitez à");
-        $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?markers='+document.querySelector("#dn").val()+'&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/>');
-
+        $(".modal-title").text( "Bienvenue  "+ document.querySelector("#prenom").value);
+        $(".modal-body").text( "vous etes nés le"+ document.querySelector("#dn").value+" et vous habitez à");
+$(".modal-body").html( "vous etes nés le "+ document.querySelector("#dn").value+" </br>et vous habitez à:"+'</br><img src="https://maps.googleapis.com/maps/api/staticmap?markers='+document.querySelector("#adresse").value+'&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/>');
 
     }
      // Y mettre le code jQuery pour valider tous les champs du formulaire
