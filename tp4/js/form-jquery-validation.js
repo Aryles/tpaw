6e2d5b22dd1882bf9f2d82bf3b92bac4585baf3c
Aryles <talbinearyles@gmail.com>
2019-02-09 21:41:27 +0100

    Cv final

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -18,7 +18,71 @@ $(document).keypress(function(event){
     var msg2 = nombreCaractere2 + ' car';
     $('#c2').text(msg2);
   });
+ 
 
+ $("#valider").on("click",function store(event) { 
+                event.preventDefault();
+                
+                    var inputNom= document.getElementById("name");
+                    var inputPrenom= document.getElementById("firstname");
+                    var inputDn= document.getElementById("birth");
+                    var inputAdresse= document.getElementById("adresse");
+                    var inputEmail= document.getElementById("mail");
+                    
+                    if($("#name").val() !== "" && $("#firstname").val() !== "" && $("#birth").val() !== "" && $("#adresse").val() !== "" && $("#mail").val() !== "" ){
+                    
+                    //stocker les valeurs saisie dans le navigateur
+                    localStorage.setItem("name", inputNom.value);
+                    localStorage.setItem("firstname", inputPrenom.value);
+                    localStorage.setItem("birth", inputDn.value);
+                    localStorage.setItem("adresse", inputAdresse.value);
+                    localStorage.setItem("mail", inputEmail.value);
+
+                   $('#success').addClass("alert alert-success").text("Bravo! le formulaire est sauvegardé.");
+
+                  $("#table").show();
+                  $("#liste_des_contacts").show();
+                  
+                   
+                   // ajout des valeurs saisie dans le tableau
+                   /*
+                   document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
+                   .innerHTML +'<tr><td>'+localStorage.getItem("name")+'</td><td>'+localStorage.getItem("firstname") +'</td><td>'+localStorage.getItem("birth")+'</td><td><a href="https://maps.google.com/maps?q=' +localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse") +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
+                   }
+                  
+                   document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
+                   .innerHTML +'<tr><td>'+contactStore.getList.name.val()+'</td><td>'+localStorage.getItem("firstname") +'</td><td>'+localStorage.getItem("birth")+'</td><td><a href="https://maps.google.com/maps?q=' +localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse") +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
+                   */ 
+                }
+                   
+                  
+            });
+            
+            $("#gps").on("click",function() { 
+                getLocation();
+                
+            });
+
+            $("#valider").on("click",function() { 
+                contactStore.add()
+
+                var contactList = contactStore.getList();
+                console.log(contactList)
+                
+                for(var index in contactList){
+                    console.log(contactList[index].name);
+                    document.querySelector("table tbody").innerHTML = 
+                    document.querySelector("table tbody").innerHTML +
+                    '<tr>'+
+                        '<td>'+contactList[index].name+'</td>'+
+                        '<td>'+contactList[index].firstname+'</td>'+
+                        '<td>'+contactList[index].date+'</td>'+
+                   /*     '<td>'+contactList[index].adress+'</td>'+   */
+                        '<td><a href="https://maps.google.com/maps?q=' +contactList[index].adress+'">' + contactList[index].adress +'</a></td>'+
+                        '<td>'+contactList[index].mail+'</td>'+
+                    '<tr>';
+                  }
+            });
 
 
 
