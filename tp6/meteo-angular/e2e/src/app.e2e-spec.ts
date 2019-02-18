882ab6423ed04630937d24acd81a99088f4bae87
Aryles <talbinearyles@gmail.com>
2019-02-18 23:44:02 +0100

    frr

--- tp6/meteo-angular/e2e/src/app.e2e-spec.ts
+++ tp6/meteo-angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to meteo-angular!');
+    expect(page.getTitleText()).toEqual('Welcome to meteo-angular2!');
   });
 
   afterEach(async () => {
